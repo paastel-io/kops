c24534f48bd40f77b3bdd778f000fd5ea8cbc329
murilo ijanc <murilo@ijanc.org>
2025-11-28 16:03:19 -0300

    Change kops_aws_sso from bin to lib

--- crates/kops_aws_sso/src/lib.rs
+++ crates/kops_aws_sso/src/lib.rs
@@ -1,41 +1,29 @@
-#![allow(unused_imports)]
+//
+// Copyright (c) 2025 murilo ijanc' <murilo@ijanc.org>
+//
+// Permission to use, copy, modify, and distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+//
+
 use std::time::SystemTime;
 
 use anyhow::{Context, Result, anyhow};
-use aws_sdk_sso::error::ProvideErrorMetadata;
 use aws_config::SdkConfig;
-use aws_credential_types::{Credentials, provider::ProvideCredentials};
+use aws_credential_types::Credentials;
 use aws_sdk_sso as sso;
+use aws_sdk_sso::error::ProvideErrorMetadata;
 use aws_sdk_ssooidc as ssooidc;
 use chrono::{DateTime, Duration, Utc};
 use tokio::time::sleep;
-use aws_sdk_ssooidc::error::SdkError;
-use aws_sdk_ssooidc::operation::create_token::CreateTokenError;
-use aws_sdk_ssooidc::types::error::{AuthorizationPendingException, SlowDownException};
-
-
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    let sso_cfg = SsoLoginConfig {
-        region: "us-east-1".into(),
-        start_url: "https://xxxx".into(),
-        account_id: "xxxxx".into(),
-        role_name: "XXXX".into(),
-        client_name: "kopsd".into(),
-    };
-
-    let sdk_config_for_sso_region = aws_config::from_env().load().await;
-
-    let session = login_device_flow(&sdk_config_for_sso_region, &sso_cfg, |info| {
-        println!("SSO user code: {}", info.user_code);
-        println!("Open URL: {}", info.verification_uri);
-    })
-    .await?;
-
-    dbg!(session);
-
-    Ok(())
-}
 
 #[derive(Debug, Clone)]
 pub struct SsoLoginConfig {
